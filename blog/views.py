8af8a4f65bafb7d3bce696ac6d1ef5ca36e00a31
mra <mohammad.reza.alizadeh3030@gmail.com>
2022-05-21 23:25:28 +0430

    Initial Commit

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def starting_page(request):
+    return render(request)
+
+
+def posts(request):
+    return render(request)
+
+
+def single_post(request):
+    pass
+
